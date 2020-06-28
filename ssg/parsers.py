2cc663e9cf4b11c5351dafe92531867c2cfc8815
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-28 14:38:48 +0200

    parser configuraton

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -8,18 +8,18 @@ class Parser:
     
     extensions: List[str] = []
 
-    def valid_extensions(self, extension):
+    def valid_extension(self, extension):
         return extension in self.extensions
     
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
     def read(self, path):
-        with open(paht, "r") as file:
+        with open(path, "r") as file:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
-        full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
     
